eac68a89cbca8c4a26fe4e1007ebec6352707a8c
kuzmin.serega@gmail.com <kuzmin.serega@gmail.com>
2015-02-17 02:29:51 +0300

    first commit yii2 inexes application

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=myinexes_yii2',
+    'username' => 'root',
+    'password' => '111',
+    'charset' => 'utf8',
+];
